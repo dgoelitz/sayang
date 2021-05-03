04796c9a8374b1b51adff2a19692f751535800ea
David Goelitz <davidgoelitz@tests-MacBook-Pro.local>
2021-05-03 00:44:30 -0700

    deploying the night before her birthday!`

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import cake from './27cake.jpg';
+import arvi1 from './arvi1.jpg';
+import arvi2 from './arvi2.jpg';
+import arvi3 from './arvi3.jpg';
+import arvi4 from './arvi4.jpg';
+import arvi5 from './arvi5.jpg';
+import arvi6 from './arvi6.jpg';
+import arvi7 from './arvi7.jpg';
+import arvi8 from './arvi8.jpg';
+import arvi9 from './arvi9.jpg';
+import arvi10 from './arvi10.jpg';
+import arvi11 from './arvi11.jpg';
+import arvi12 from './arvi12.jpg';
+import arvi13 from './arvi13.jpg';
+import arvi14 from './arvi14.jpg';
+import arvi15 from './arvi15.jpg';
+import arvi16 from './arvi16.jpg';
+import arvi17 from './arvi17.jpg';
+import arvi18 from './arvi18.jpg';
+import arvi19 from './arvi19.jpg';
+import arvi20 from './arvi20.jpg';
+import arvi21 from './arvi21.jpg';
+import arvi22 from './arvi22.jpg';
+import arvi23 from './arvi23.jpg';
+import arvi24 from './arvi24.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Happy birthday, sayang!</h1>
+        <p>I love you so much, baby! I am so grateful that you spent your 26th year with me! It was the happiest year of my life, having you finally come home so we can be together everyday for the rest of our lives! I hope you had a good 26th year, starting this life with me!</p>
+        <img src={cake} alt="27th birthday cake"></img>
+        <p>And what a year it was!</p>
+        <div className="row">
+          <img src={arvi1} alt="memory from the last year" className="pic"></img>
+          <img src={arvi2} alt="memory from the last year" className="pic"></img>
+          <img src={arvi3} alt="memory from the last year" className="pic"></img>
+          <img src={arvi4} alt="memory from the last year" className="pic"></img>
+          <img src={arvi5} alt="memory from the last year" className="pic"></img>
+          <img src={arvi6} alt="memory from the last year" className="pic"></img>
+          <img src={arvi7} alt="memory from the last year" className="pic"></img>
+          <img src={arvi8} alt="memory from the last year" className="pic"></img>
+        </div>
+        <div className="row">
+          <img src={arvi9} alt="memory from the last year" className="pic"></img>
+          <img src={arvi10} alt="memory from the last year" className="pic"></img>
+          <img src={arvi11} alt="memory from the last year" className="pic"></img>
+          <img src={arvi12} alt="memory from the last year" className="pic"></img>
+          <img src={arvi13} alt="memory from the last year" className="pic"></img>
+          <img src={arvi14} alt="memory from the last year" className="pic"></img>
+          <img src={arvi15} alt="memory from the last year" className="pic"></img>
+          <img src={arvi16} alt="memory from the last year" className="pic"></img>
+        </div>
+        <div className="row">
+          <img src={arvi17} alt="memory from the last year" className="pic"></img>
+          <img src={arvi18} alt="memory from the last year" className="pic"></img>
+          <img src={arvi19} alt="memory from the last year" className="pic"></img>
+          <img src={arvi20} alt="memory from the last year" className="pic"></img>
+          <img src={arvi21} alt="memory from the last year" className="pic"></img>
+          <img src={arvi22} alt="memory from the last year" className="pic"></img>
+          <img src={arvi23} alt="memory from the last year" className="pic"></img>
+          <img src={arvi24} alt="memory from the last year" className="pic"></img>
+        </div>
       </header>
     </div>
   );
